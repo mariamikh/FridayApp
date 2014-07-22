bb24be7223cfbedb142864cf56b4d83b3a3dc984
Mariam <m.khunjgurua@gmail.com>
2014-07-22 11:13:29 +0400

    add missed files, change api Key

---
+++ src/ge/ufc/atm/DisplayMapActivity.java
@@ -0,0 +1,13 @@
+package ge.ufc.atm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DisplayMapActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display_map);
+	}
+}
