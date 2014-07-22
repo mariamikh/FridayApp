1615b9d3b033a92b02bf666e7d36037f9b94b8bb
Mariam <m.khunjgurua@gmail.com>
2014-07-22 13:11:25 +0400

    add target version in manifest; before testing start internet connection on your mobile, bu he he

--- src/ge/ufc/atm/DisplayMapActivity.java
+++ src/ge/ufc/atm/DisplayMapActivity.java
@@ -2,6 +2,7 @@ package ge.ufc.atm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class DisplayMapActivity extends Activity {
 
@@ -10,4 +11,11 @@ public class DisplayMapActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_map);
 	}
+	
+	 public boolean onCreateOptionsMenu(Menu menu) 
+	    {
+	        // Inflate the menu; this adds items to the action bar if it is present.
+	     //   getMenuInflater().inflate(menu.activity_map, menu);
+	        return true;
+	    }
 }
